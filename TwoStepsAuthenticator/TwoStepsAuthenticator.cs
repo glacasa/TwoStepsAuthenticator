d3ac57ba63e266e1edc5d503b602c7ff50618056
Guillaume Lacasa <guillaume.lacasa@ucaya.com>
2013-06-05 18:43:45 +0200

    création du projet

---
+++ TwoStepsAuthenticator/TwoStepsAuthenticator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStepsAuthenticator
+{
+    public class TwoStepsAuthenticator
+    {
+        public void GetCode(string secret, DateTime date)
+        {
+            var seconds = Math.Floor(time.ToUnixTime() / 30);
+
+
+        }
+    }
+}
---
+++ TwoStepsAuthenticator/UnixTimeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStepsAuthenticator
+{
+    // http://www.rudyhuyn.com/blog/2012/04/02/convertir-un-temps-unix-vers-un-datetime-et-inversement/
+    internal static class UnixTimeHelper
+    {
+        internal static DateTime ToDateTime(this long unixTime)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixTime).ToLocalTime();
+        }
+
+        internal static long ToUnixTime(this DateTime datetime)
+        {
+            TimeSpan ts = (datetime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+            return (long)ts.TotalSeconds;
+        }
+    }
+}
