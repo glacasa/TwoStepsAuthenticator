f83d2a6a47734300a265273328f31120c38941e8
Guillaume Lacasa <guillaume.lacasa@ucaya.com>
2013-06-12 18:42:34 +0200

    on avance dans l'algo

---
+++ TwoStepsAuthenticator.TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStepsAuthenticator.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var auth = new TwoStepsAuthenticator();
+            auth.GetPinCode("JBSWY3DPEHPK3PXP", new DateTime(2013, 6, 12, 18, 0, 0));
+        }
+    }
+}
--- TwoStepsAuthenticator/TwoStepsAuthenticator.cs
+++ TwoStepsAuthenticator/TwoStepsAuthenticator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,34 @@ namespace TwoStepsAuthenticator
 {
     public class TwoStepsAuthenticator
     {
-        public void GetCode(string secret, DateTime date)
+        //private static final int PASS_CODE_LENGTH = 6;
+        ///** Default passcode timeout period (in seconds) */
+        //private static final int INTERVAL = 30;
+        HMACSHA1 mac;
+
+        public void GetPinCode(string key, DateTime date)
+        {            
+            TimeSpan ts = (date.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+            var interval = (long)ts.TotalSeconds / 30;
+
+            var challengeBytes = BitConverter.GetBytes(interval);
+            // OK jusque là
+
+
+
+            mac = new HMACSHA1(Base32Encoding.ToBytes(key));
+
+            var hash = mac.ComputeHash(challengeBytes);
+            //var hash = mac.TransformFinalBlock(challengeBytes);
+            //int offset = hash[hash.Length - 1] & 0xF;
+            //int truncatedHash = hashToInt(hash, offset) & 0x7FFFFFFF;
+        }
+
+
+
+        private int hashToInt(byte[] bytes, int start)
         {
-            var seconds = Math.Floor(time.ToUnixTime() / 30);
-
-
+            return BitConverter.ToInt32(bytes, start);
         }
     }
 }
